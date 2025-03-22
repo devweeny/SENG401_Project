e610e110761ca6feadd785af295e6ee5204e9bbb
devweeny <dylanwenaas@gmail.com>
2025-03-21 22:25:22 -0600

    Test fix fr

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,14 +1,6 @@
-module.exports = {
-  presets: [
-    "module:@react-native/babel-preset",
-    "@babel/preset-flow",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-  ],
-  plugins: [
-    "@babel/plugin-transform-flow-strip-types",
-    ["@babel/plugin-transform-private-methods", { loose: true }],
-    ["@babel/plugin-transform-class-properties", { loose: true }],
-    ["@babel/plugin-transform-private-property-in-object", { loose: true }],
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
